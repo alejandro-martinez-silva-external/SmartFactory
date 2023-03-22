40aa006b639c78f8ed1f2cac1efc45e66d793d04
alejandro-martinez-silva-external <1684+alejandro-martinez-silva-external@users.noreply.github.capgemini.com>
2023-03-22 11:50:37 +0100

    New FBX + Pause Menu

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Video;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pausePanel;
+    [SerializeField] GameObject settingsPanel;
+
+    [SerializeField] GameObject leftReticle;
+    [SerializeField] GameObject rightReticle;
+    
+    public Transform player;
+    public float maxDistance = 10.0f;
+    public float maxHeight;
+    public float rotationSpeed = 0.1f;
+    [SerializeField] private GameObject tunnelingEffect;
+
+    void Start(){
+    pausePanel.SetActive(false);
+    settingsPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            UpdatePauseMenu();
+        }
+    }
+
+    void LateUpdate(){
+        // Mueve el Canvas al centro de la pantalla
+        Vector3 centerScreen = new Vector3(Screen.width / 2, Screen.height / 2, maxDistance);
+        Vector3 targetPos = Camera.main.ScreenToWorldPoint(centerScreen);
+        
+        if (targetPos.y < maxHeight)
+        {
+            targetPos.y = maxHeight;
+        }
+
+        transform.position = targetPos;
+
+        // Haz que el Canvas mire hacia el jugador
+        Vector3 targetPosition = player.position + Vector3.up;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(transform.position - targetPosition), rotationSpeed);
+    }
+
+    void UpdatePauseMenu(){
+        pausePanel.SetActive(!pausePanel.activeSelf);
+        settingsPanel.SetActive(false);
+        //leftReticle.SetActive(!leftReticle.activeSelf);
+        //rightReticle.SetActive(!rightReticle.activeSelf);
+    }
+
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+    public void BackToGame()
+    {
+        UpdatePauseMenu();
+    }
+
+    public void UpdateTunneling(){
+        tunnelingEffect.SetActive(!tunnelingEffect.activeSelf);
+    }
+}
